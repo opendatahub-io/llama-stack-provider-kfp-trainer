f8dc8e67b9c3d3276d02cd2cf2042285fcdb350c
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-06-04 18:27:53 -0400

    Install some missing deps for lls-client
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- scripts/run-training.sh
+++ scripts/run-training.sh
@@ -4,5 +4,8 @@ python -m venv .venv-client
 . .venv-client/bin/activate
 pip install llama-stack-client
 
+# Client fails to pull some dependencies that it imports
+pip install fire requests
+
 export INFERENCE_MODEL=Llama-3.2-3B-Instruct
 python train.py
